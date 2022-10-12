3ba9beb7e8377a146c4871d9f09249dbc767ae98
jackson- <jackson.t.devin@gmail.com>
2022-10-12 02:17:58 -0400

    display name and image pulled in and displayed. need to grab username now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,117 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState, useEffect } from "react";
+
+import { getAuth, TwitterAuthProvider, signInWithPopup } from "firebase/auth";
+
+import { initializeApp } from "firebase/app";
+
+
+const provider = new TwitterAuthProvider();
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+};
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp)
 
 const Home: NextPage = () => {
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [currentUser, setCurrentUser] = useState<any | null>(null);
+
+  const connectTwitter = async () => {
+      const provider = new TwitterAuthProvider();
+      
+      signInWithPopup(auth, provider)
+      .then((re) => {
+        const updatedUser = {name: re.user.displayName, image: re.user.photoURL, prevState: currentUser}
+        setCurrentUser(updatedUser);
+      }).catch((err) => {
+        console.log("ERR ", err);
+      })
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      {/* May want to account for browsers that have wallets built in like Brave */}
+      if (!ethereum) {
+        alert("Get MetaMask -> https://metamask.io/");
+        return;
+      }
+			
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const checkIfWalletIsConnected = async () => {
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log('Make sure you have metamask!');
+			return;
+		} else {
+			console.log('We have the ethereum object', ethereum);
+		}
+
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			setCurrentAccount(account);
+		} else {
+			console.log('No authorized account found');
+		}
+	};
+
+  // Render methods
+	const renderNotConnectedContainer = () => (
+		<div className="connect-wallet-container">
+			<button onClick={connectWallet} className="cta-button connect-wallet-button">
+				Connect Wallet
+			</button>
+		</div>
+	);
+
+  const renderTwitterSignup = () => {
+    return (
+      <div className="twitter-container">
+        <button onClick={connectTwitter}>Sign in with Twitter</button>
+      </div>
+    );
+  }
+
+  const renderTwitterInfo = () => {
+    return (
+      <div className="twitter-container">
+       <h1>Welcome back {currentUser.name}</h1>
+       <img src={currentUser.image} />
+      </div>
+    );
+  }
+
+  useEffect(() => {
+		checkIfWalletIsConnected();
+	}, []);
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+    <div className="App">
+			<div className="container">
+				{!currentAccount && renderNotConnectedContainer()}
+				{/* Render the input form if an account is connected */}
+				{currentAccount && !currentUser && renderTwitterSignup()}
+        {currentAccount && currentUser && renderTwitterInfo()}
+			</div>
+		</div>
   )
 }
 
